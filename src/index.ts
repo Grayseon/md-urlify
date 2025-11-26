8c706bb24e2833e43ea3a02e56543ef084632669
Grayseon <grayseon@gmail.com>
2025-11-25 22:01:05 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { Hono } from "hono"
+
+const app = new Hono()
+
+function convertUrls(text: string, prefix: string = "URL") {
+  const regex = /\b(?:https?:\/\/)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\/\S*)?/g;
+  const matches = text.match(regex) || [];
+  let count = 1;
+  let result = text;
+
+  for (const m of matches) {
+    const full = m.startsWith('http') ? m : 'https://' + m;
+    const escaped = m.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+    const title = prefix + " " + count++;
+    result = result.replace(new RegExp(escaped, 'g'), `(${title})[${full}]`);
+  }
+
+  return result;
+}
+
+
+app.get("/", async (c) => {
+  const encodedText = c.req.query("text")
+  const prefix = c.req.query("prefix")
+  if (!encodedText)
+    return c.text(
+      "Welcome to md-urlify. Enter a base-64 encoded text into a text URL param to turn URLs into MD URLs."
+    )
+  const text = atob(encodedText)
+
+  return c.text(convertUrls(text, prefix))
+})
+
+export default app
